aa3276a66bdaffff6e03e51acf7b14fb112a5123
raiana773 <104357057+raiana773@users.noreply.github.com>
2023-07-15 02:57:36 +0545

    lesson

---
+++ js/main.js
@@ -0,0 +1,129 @@
+// ! let, var, const
+// console.log("Hello world!");
+//  let, var, const
+
+//  LET
+//  console.log(firstName);   Connot access 'firstName'
+// let firstName = "Kani";
+// firstName= "Aziret";
+// console.log(firstName);
+
+//  VAR -объявление переменны
+// console.log(num);
+// var num = 10;
+// num =20;
+// console.log(num);
+
+//  CONST
+// console.log(PI);   Assignment to constant variable.
+// const  PI = 3.14;
+// PI = 999;
+// Assignment to constant variable.
+
+//  dane types
+// 1 string
+// "", '',
+// let str = "stroka";
+// console.log(typeof str);
+
+// numer
+// let num1 = 100;  //numer
+// let num2 = 2.5;  //float numer
+
+// .............................3 boolean
+// let bool1 = true;
+// let bool2 = false;
+
+//............................... 4 null
+// let products = null;
+
+//............................... 5 undedined
+// let elements;
+
+//................................. 6 (object, array) ссылочный тип данных
+// let obj = {
+//     name: "John"
+// }
+// let obj2 = obj;
+// console.log(obj.name);
+// console.log(obj.name);
+// // console.log(obj. ['name']);
+
+// let arr = [true, {}, 100, "str"]
+
+// ............................................7 bigInt
+// let bigNum = 1313n;
+// console.log(bigNum);
+
+//................................ 8 symbol
+let id = Symbol("id18378913");
+
+//console.log("Hello World!");
+
+// let example = 'Hello World 2';
+// const examle2 = 'Hello World 3';
+
+// console.log(example, examle2);
+// //console.log(examle2);
+
+// let name = 'Kani';
+// name = 'Makers';
+// console.log(name);
+
+// const name = 'Kani';
+// //name = 'Makers'
+// console.log(name)
+
+//1.STRING-строки
+//  let str = 'Hello World';
+//  let str1 = 'Hello';
+//  let str2 = '5';
+//  console.log(str, str1, str);
+
+// //2. NUMBER-НОМЕР
+// let num =25;
+// let num2 =12.25;
+// console.log(num, num2);
+
+//3. BOOLEAN-ЛОГИЧЕСКИЙ ТИП ДАННЫХ
+// let bool1 = true;  //истинна или же да
+// let bool2 = false;  //лож или нет
+// console.log(bool1);
+// console.log(bool2);
+
+// console.log(Boolean(0));
+
+//4.UNDEFINED-НЕ СПЕЦИАЛЬНАЯ ПУСТОТА
+// let a
+// console.log(a)
+
+//5.NULL-СПЕЦИАЛЬНАЯ ПУТОТА
+// let b = null;
+// console.log(b);
+
+//6.OBJECT-ОБЪЕКТ
+// let obj ={
+//     name:'Kani',
+//     'last-name': 'Arstanbaeva',
+//     age: 22,
+//     hobby: null,
+//     isHuman: true,
+//     dog: {
+//         name: 'Oreo',
+//         color: 'black-white'
+//     }
+// }
+// console.log(obj);
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log(obj["last-name"]);
+// console.log(obj.isHuman);
+// console.log(obj.dog.name);
+// console.log((obj.dog.color));
+
+// ARRAY-МАССИВ ОБЪКТ
+
+// let arr =[1, 2, 3, 4, true, false, undefined, null, 'hello',{name: 'Kani'}, [7, 8, 9]];
+// console.log(arr);
+// console.log(arr[4]);
+// console.log(arr[8]);
